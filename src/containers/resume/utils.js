d280119e81b923f7d87d685124dbe362b13f4412
tanjabodunrin@gmail.com <tanjabodunrin@gmail.com>
2022-12-06 00:58:52 +0100

    second changes

--- src/containers/resume/utils.js
+++ src/containers/resume/utils.js
@@ -1,51 +1,45 @@
-export const data ={
-    experience: [{
-        title: "Self Taught,",
-        subTitle:" Online",
-        description:'Python Programming Language',
-        date:"Jan 2022 - Present",
+export const data = {
+  experience: [
+    {
+      title: "Self Taught,",
+      subTitle: " Online",
+      description: "Python Programming Language",
+      date: "Jan 2022 - Present",
     },
 
     {
-        title: "NYSC,",
-        subTitle:" Alimosho,Lag",
-        description:'Front Desk Attendant',
-        date:"2021 - 2022",
+      title: "NYSC,",
+      subTitle: " Alimosho,Lag",
+      description: "Front Desk Attendant",
+      date: "2021 - 2022",
     },
 
     {
-        title: "Operational Manager,",
-        subTitle:" Egbeda,Lag",
-        description:'Supervisor',
-        date:"2021 - present",
+      title: "Operational Manager,",
+      subTitle: " Egbeda,Lag",
+      description: "Supervisor",
+      date: "2021 - present",
     },
+  ],
 
-    ],
-    
-    education: [
+  education: [
     {
-        title: 'Student,',
-        subTitle:'TechStudio Academy',
-        description: 'Python Full Stack Web Development',
-        date:"July,2022 - October,2022 ",
-
-
+      title: "Student,",
+      subTitle: "TechStudio Academy",
+      description: "Python Full Stack Web Development",
+      date: "July,2022 - October,2022 ",
     },
     {
-        title: 'University,',
-        subTitle:' Centre International Universitaire Des Meilleurs',
-        description: '3.98 CGPA',
-        date:"2017 - 2020",
-
+      title: "University,",
+      subTitle: " Centre International Universitaire Des Meilleurs",
+      description: "Computer Science(Second Class Upper)",
+      date: "2017 - 2020",
     },
     {
-        title: 'Secondary,',
-        subTitle:'Arisona Private College',
-        description: 'Distinction',
-        date:"2009 - 2015",
+      title: "Secondary,",
+      subTitle: "Arisona Private College",
+      description: "Science Department(Distinction)",
+      date: "2009 - 2015",
     },
-
-
-
-    ],
+  ],
 };
